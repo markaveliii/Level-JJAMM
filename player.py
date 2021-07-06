59f81fdeb2fe4e0b3e765d7c76846f94e61fd9f4
Anthony Chin <canthony@ada.cs.pdx.edu>
2021-07-06 14:33:48 -0700

    added getter and setter for sword in inventory

--- player.py
+++ player.py
@@ -4,3 +4,15 @@ class Player:
 	def __init__(self, x_pos, y_pos):
 	    self.x_pos = x_pos
 	    self.y_pos = y_pos
+
+        #check inventory for sword
+        def get_sword(self):
+            found = False
+            for item in self.inventory:
+                if item = 'sword':
+                    found = True
+            return found
+
+        #insert sword into inventory
+        def set_sword(self):
+            self.inventory.append('sword')
