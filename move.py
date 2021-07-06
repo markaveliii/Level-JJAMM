631bf6f2a784a34fca1f94731021f46fe496053b
Mark Montes <mark-montes@hotmail.com>
2021-07-06 13:23:44 -0700

    Added player class. Started move function for player.

--- move.py
+++ move.py
@@ -1,6 +1,19 @@
+
+import player
 # This is for moving any animate creature in Level-JJAMM in the map
 
-def move(map, creature, ix, iy, fx, fy):
-    map.objArr[ix, iy] = '-'
-    map.objArr[fx, fy] = creature
-    return map
+def move(creature, keystroke):
+
+    if keystroke == 'w':
+        creature.y_pos -= 1
+    elif keystroke == 'a':
+        creature.x_pos -=1
+    elif keystroke == 's':
+        creature.y_pos += 1
+    elif keystroke == 'd':
+        creature.x_pos += 1
+
+p1 = player.Player(0,0)
+move(p1, 'w')
+
+print(p1.x_pos, ", ", p1.y_pos)
