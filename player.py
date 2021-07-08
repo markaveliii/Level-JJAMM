671a19948bf29235e2266b8c7359f3d804a975cf
Mark Montes <mark.montes44@gmail.com>
2021-07-08 13:29:08 -0700

    Added test bounds on move.

--- player.py
+++ player.py
@@ -18,9 +18,15 @@ class Player:
         self.inventory.append('sword')
 
     #moves players x,y position on map
-    def move(self, keystroke):
+    def move(self, keystroke, map):
         if keystroke == 'w':
-            self.y_pos -= 1
+            if self.y_pos-1 <= 0:
+                return
+            if map[self.x_pos][self.y_pos-1] == 'w':
+                return
+            else:
+                self.y_pos -= 1
+
         elif keystroke == 'a':
             self.x_pos -=1
         elif keystroke == 's':
