408396678ea6d53506a39f7b5662d1516e8d9f22
Melissa Barnes <barnes28@gmail.com>
2021-07-11 17:27:25 -0700

    Messed with colors, added comments and cleaned up menu class

--- menu.py
+++ menu.py
@@ -1,65 +1,40 @@
-#!/usr/bin/python3
+#This file contains methods to display menu, timer and lives of the player all in a Menu class. The display
+#is refreshed each time the player hits a key. Lives are lost or won through gameplay. 
 
 import curses, curses.panel
 from curses import wrapper
 import time
 
 class Menu:
-    def __init__(self):
+    def __init__(self):                             #holds level start time to calculate time for timer
             self.start_time = round(time.time(),2)
-    
-    def reset_timer(self):
+
+    def reset_timer(self):                              #resets timer for new level
             self.start_time = round(time.time(),2)
             pass
 
-    def display_menu(self, begin_x): # initially passed in stdscr
-            # begin_x = 90;
+    def display_menu(self, begin_x):                #Displays menu border, time and lives
             stdscr = curses.initscr()
-            begin_y = 0;
-            height = 5;
-            width = 40;
+            curses.start_color()
+            curses.init_pair(9, curses.COLOR_BLACK, curses.COLOR_MAGENTA)
+            curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_BLACK)
+            begin_y = 3
+            height = 5
+            width = 70
             win = curses.newwin(height, width, begin_y, begin_x+2)
             livesNum = 3;
             lives = "Lives: " + str(livesNum)
             title = "Menu:"
             current_time = round(time.time(),2)
             time_elapsed = "Time Elapsed: " + str(round((current_time - self.start_time),2))
-            # stdscr.clear()
             stdscr.refresh()
-            for i in range(0, 20):
-                    stdscr.addch(begin_y,begin_x+i, "#") 
-                    stdscr.addch(begin_y+19,begin_x+i, "#") 
-            for i in range(0, 20):
-                    stdscr.addch(begin_y+i,begin_x, "#") 
-                    stdscr.addch(begin_y+i,begin_x+19, "#") 
-            stdscr.addstr(begin_y+5, begin_x+5, title);
-            stdscr.addstr(begin_y+6, begin_x+5, lives);
-            stdscr.addstr(begin_y+7, begin_x+5, time_elapsed);
-            # stdscr.refresh();
-            # k = stdscr.getch()
-    
-            # curses.endwin();
+            for i in range(0, 25):        #printing horizontal border
+                    stdscr.addch(begin_y,begin_x+i, "*", curses.color_pair(9)) 
+                    stdscr.addch(begin_y+9,begin_x+i, "*", curses.color_pair(9)) 
+            for i in range(0, 10):        #printing vertical border
+                    stdscr.addch(begin_y+i,begin_x, "*", curses.color_pair(9)) 
+                    stdscr.addch(begin_y+i,begin_x+24, "*", curses.color_pair(9)) 
+            stdscr.addstr(begin_y+2, begin_x+1, title,curses.color_pair(10))   #printing title, lives, time
+            stdscr.addstr(begin_y+3, begin_x+1, lives, curses.color_pair(10))
+            stdscr.addstr(begin_y+4, begin_x+1, time_elapsed, curses.color_pair(10))
             pass
-
-
-
-    def testMenu(self):
-        game_menu = Menu() 
-        #https://stackoverflow.com/questions/28751080/python-curses-typeerror-when-exiting-wrapper
-        # stdscr = curses.initscr()
-        try: 
-                Menu.display_menu(game_menu, stdscr)
-                Menu.display_menu(game_menu, stdscr)
-                Menu.display_menu(game_menu, stdscr)
-                Menu.display_menu(game_menu, stdscr)
-                Menu.display_menu(game_menu, stdscr)
-                
-        except:
-                curses.endwin()
-                raise
-        else:
-                curses.endwin()
-#wrapper(Menu.display_menu(game_menu, stdscr))
-#time.sleep(5)
-#wrapper(Menu.display_menu(game_menu, stdscr))
-#curses.endwin()
